b916f18d5196e4476f6f218a79dbc0cb1e64c9fa
BeautyOnCode <nguyendiemthanhdn92@gmail.com>
2022-08-02 13:45:23 +0700

    Time: 31 ms (26.38%), Space: 13.3 MB (59.63%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -10,16 +10,16 @@ class Solution(object):
         :rtype: int
         """
         left, right = 1, n
-        
+
         while left < right:
-            
-            mid = left + (right - left) // 2
-            
+
+            mid = (left + right) // 2
+
             if isBadVersion(mid):
                 right = mid
             else:
                 left = mid + 1
-                
+
         return left
-        
+
         

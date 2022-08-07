c8f8f942cbc3d0889e1f258790ef33859daa6b5d
BeautyOnCode <nguyendiemthanhdn92@gmail.com>
2022-08-07 21:12:46 +0700

    Time: 109 ms (35.12%), Space: 44.6 MB (43.60%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.ts
+++ 35-search-insert-position/35-search-insert-position.ts
@@ -1,43 +1,7 @@
-function searchInsert(nums: number[], target: number): number {
-    const len = nums.length;
-    if(len == 0) return -1;
-    
-    // range search will be from 0 to len of the nums
-    let l = 0, r = nums.length - 1;
-    
-    while(l < r) {
-        // middle will = (left + right) // 2
-        // use left + (right - left) // 2 to avoid overflow
-        let m = l + (r - l) // 2;
-        
-        // if equal target, return index
-        if (nums[m] == target) {
-            return m;
-        }
-        
-        if (nums[m] < target) {
-            // if middle value smaller then target
-            // move the left to middle index
-            l = m;
-        } else {
-            // if middle value bigger then target
-            // move the right to before the middle
-            // (exclude the middle because already check)
-            r = m - 1;
-        }
+var searchInsert = function(nums, target) {
+    let count = 0
+    while (nums[count] < target) {
+        count++
     }
-
-    if (nums[r] == target) return r;
-    if (nums[l] == target) return l;
-    // if not found equal then return the closest index
-    // where could insert
-    if (nums[r] > target) {
-        // there is special case where r is 0 then return 0 instead of -1
-        if (r == 0) return 0
-        
-        return r - 1
-    } else {
-        return l + 1
-    }
-    
+    return count
 };

ce7b084055e8c790b5f09a43d33b9d30682566ae
BeautyOnCode <nguyendiemthanhdn92@gmail.com>
2022-08-07 22:10:21 +0700

    Time: 89 ms (69.59%), Space: 44.3 MB (70.81%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.ts
+++ 35-search-insert-position/35-search-insert-position.ts
@@ -1,28 +1,30 @@
 function searchInsert(nums: number[], target: number): number {
-    const len = nums.length;
-    if(len == 0) return -1;
-    if (len == 1) {
-        if (nums[0] < target) {
-            return 1
-        }
-        return 0
-    }
+    // const len = nums.length;
+    // if (len == 0) return -1;
+    // if (len == 1) {
+    //     if (nums[0] < target) {
+    //         return 1
+    //     }
+    //     return 0
+    // }
     
     // range search will be from 0 to len of the nums
     let l = 0, r = nums.length - 1;
-    
-    while(l < r) {
+
+    while(l <= r) {
         // middle will = (left + right) // 2
         // use left + (right - left) // 2 to avoid overflow
-        let m = l + (r - l) // 2;
-        
+        let m = l + (r - l) % 2;
+
         // if equal target, return index
         if (nums[m] == target) {
             return m;
-        } else if (nums[m] < target) {
+        }
+        
+        if (nums[m] < target) {
             // if middle value smaller then target
-            // move the left to middle index
-            l = m;
+            // move the left to middle + 1 index
+            l = m + 1;
         } else {
             // if middle value bigger then target
             // move the right to before the middle
@@ -30,19 +32,6 @@ function searchInsert(nums: number[], target: number): number {
             r = m - 1;
         }
     }
-
-    if (nums[r] == target) return r;
-    if (nums[l] == target) return l;
     
-    // if not found equal then return the closest index
-    // where could insert
-    if (nums[r] > target) {
-        // there is special case where r is 0 then return 0 instead of -1
-        if (r == 0) return 0
-        
-        return r - 1
-    } else {
-        return l + 1
-    }
-    
+    return l  
 };

9a9d3ef1abbb2bfa4b5ef3d22be40f6f88789c89
BeautyOnCode <nguyendiemthanhdn92@gmail.com>
2022-09-18 16:57:45 +0700

    Time: 34 ms (88.34%), Space: 14 MB (10.79%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -0,0 +1,23 @@
+import re
+
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        word_list = s.split(" ")
+        word_dict = {}
+        for string in word_list:
+            index = re.findall(r'\d+', string)[0]
+            word_dict[index] = string.replace(index, '')
+            
+        indexes_sorted = list(word_dict.keys())
+        indexes_sorted.sort()
+        print('indexes_sorted', indexes_sorted)
+        sentence = ''
+        length_indexes_sorted = len(indexes_sorted)
+        
+        for i, index in enumerate(indexes_sorted):
+            if i == length_indexes_sorted - 1:
+                sentence += word_dict[index]
+            else:
+                sentence += word_dict[index] + " "
+            
+        return sentence

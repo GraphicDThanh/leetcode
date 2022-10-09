e28c08b5096153dae114bc379af2061a6f50e853
BeautyOnCode <nguyendiemthanhdn92@gmail.com>
2022-10-09 17:14:07 +0700

    Time: 60 ms (49.86%), Space: 14.1 MB (59.23%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.py
+++ 541-reverse-string-ii/541-reverse-string-ii.py
@@ -1,24 +1,21 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
         result_s = ""
-        index2k = []
-        len_s = len(s)
-        k_2 = 2 * k
         
-        # Find list of index break by 2k
-        for i in range(0, len_s + 1):
-            if i % k_2 == 0:
-                index2k.append(i)
-            
+        # indexed of 2k is the range from 0 to len s with step is 2k
+        indexes_2k = list(range(0, len(s) + 1, 2 * k))
+        len_indexes_2k = len(indexes_2k) 
         # Break s down to 2k, revert the first k and add to results
-        len_2k = len(index2k)
-        for key, value in enumerate(index2k):
-            if key + 1 >= len_2k:
-                k_2_s = s[index2k[key]:]
+        
+        for key, _ in enumerate(indexes_2k):
+            if key + 1 >= len_indexes_2k:
+                chunk_2k = s[indexes_2k[key]:]
             else:
-                k_2_s = s[index2k[key]:index2k[key+1]]
+                chunk_2k = s[indexes_2k[key]:indexes_2k[key+1]]
 
-            reverse_k_s = k_2_s[0:k][::-1]
-            result_s += reverse_k_s + k_2_s[k:]
+            reverse_chunk_k = chunk_2k[0:k][::-1]
+            transfer_chunk_2k = reverse_chunk_k + chunk_2k[k:]
+            
+            result_s += transfer_chunk_2k
             
         return result_s

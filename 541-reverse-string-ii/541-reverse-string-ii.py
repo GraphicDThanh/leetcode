9b906d2aa3569b24f02b1e6845e3a256af963e6f
BeautyOnCode <nguyendiemthanhdn92@gmail.com>
2022-10-09 17:15:10 +0700

    Time: 50 ms (70.58%), Space: 14 MB (92.90%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.py
+++ 541-reverse-string-ii/541-reverse-string-ii.py
@@ -5,13 +5,13 @@ class Solution:
         # indexed of 2k is the range from 0 to len s with step is 2k
         indexes_2k = list(range(0, len(s) + 1, 2 * k))
         len_indexes_2k = len(indexes_2k) 
-        # Break s down to 2k, revert the first k and add to results
         
-        for key, _ in enumerate(indexes_2k):
+        # Break s down to 2k, revert the first k and add to results
+        for key, value in enumerate(indexes_2k):
             if key + 1 >= len_indexes_2k:
-                chunk_2k = s[indexes_2k[key]:]
+                chunk_2k = s[value:]
             else:
-                chunk_2k = s[indexes_2k[key]:indexes_2k[key+1]]
+                chunk_2k = s[value:indexes_2k[key + 1]]
 
             reverse_chunk_k = chunk_2k[0:k][::-1]
             transfer_chunk_2k = reverse_chunk_k + chunk_2k[k:]

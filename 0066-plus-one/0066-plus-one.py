3360a0d296fc095cab44d11aed88f2ec5979f418
BeautyOnCode <nguyendiemthanhdn92@gmail.com>
2022-10-28 21:35:27 +0700

    Time: 29 ms (69.19%), Space: 13.4 MB (43.60%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -4,11 +4,8 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        # digits = [str(i) for i in digits]
-        # digits = str(int(''.join(digits)) + 1)
-        # return [int(i) for i in digits]
-        
         length = len(digits)
+        # carry wil have when the last item is 9
         carry, i = 1, length - 1
         
         # assume always have carry
@@ -18,14 +15,20 @@ class Solution(object):
                 value = digits[i]
             
                 if value == 9:
+                    # only kep the loop if have carry
                     digits[i] = 0
                 else:
-                    # value is not 9 then update carry to 0
+                    # value is not 9 then 
+                    # - increase value to 1
+                    # - update carry to 0
+                    # quit the loop since no carry
                     digits[i] += 1
                     carry = 0
                     
             else:
-                # exist carry then appen 1 to top
+                # out of element
+                # add 1 to top of list
+                # reset carry to 0 to quit the loop
                 digits = [1] + digits
                 carry = 0
                 
